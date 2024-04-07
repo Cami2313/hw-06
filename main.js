3a93afa7e8cc528bd0dcdcaa9f35a88d9359e40d
Camila Delgado <camiladelgado.1321@gmail.com>
2024-04-07 03:04:45 -0400

    feature(common): added lab 6

--- main.js
+++ main.js
@@ -1,12 +1,41 @@
-import './style.css'
+if (document.title === "Document") {  
+  //link
+  const link = document.querySelector("a");
+  link.href = "https://keybr.com";
 
-document.querySelector('#app').innerHTML = `
-  <div style="display: flex; flex-direction: column;">
-    <h2> HTML CSS & JS </h2>
-    <a href="./src/hw01/q01.html"> Go to the first excercise </a>
-    <a href="./src/hw02/q02.html"> Go to the second excercise </a>
-    <a href="./src/hw03/q03.html"> Go to the third excercise </a>
-  </div>
-`
+  //lista
+  const appList = document.getElementById('root');
 
-setupCounter(document.querySelector('#counter'));
+  const buyList = [
+    "books",
+    "bread",
+    "eraser",
+    "earphones",
+    "collection-cards",
+    "hdmi",
+    "vga",
+    "motherboard",
+    "university-books",
+  ];
+
+  const list = document.createElement('ul');
+
+  buyList.forEach(item => {
+    const listItems = document.createElement('li');
+    listItems.textContent = item;
+    list.appendChild(listItems);
+  });
+
+  appList.appendChild(list);
+} else {
+  document.querySelector('#app').innerHTML = `
+    <div style="display: flex; flex-direction: column;">
+      <h2> HTML CSS & JS </h2>
+      <a href="./src/hw01/q01.html"> Go to the first excercise </a>
+      <a href="./src/hw02/q02.html"> Go to the second excercise </a>
+      <a href="./src/hw03/q03.html"> Go to the third excercise </a>
+    </div>
+  `
+
+  setupCounter(document.querySelector('#counter'));
+}
---
+++ src/hw03/q03.js
@@ -0,0 +1,7 @@
+//h2
+const app = document.getElementById("root");
+
+const h2Text = document.createElement("h2");
+h2Text.textContent = "MANIPULANDO EL DOM - LAB 2";
+
+app.appendChild(h2Text);
